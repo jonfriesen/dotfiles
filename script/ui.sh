db936b2ddb081d380306da13744c3e4b5fa26870
Jon Friesen <jon@jonfriesen.ca>
2020-02-22 10:44:19 -0800

    adds spotify to UI

--- script/ui.sh
+++ script/ui.sh
@@ -4,5 +4,12 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 
+# Spotify
+snap install spotify
+
 # vscode (this should probably check if xserver or something is installed)
-# ./vscode.sh
+./vscode.sh
+apt-get install code-insiders -y
+
+# TODO add VS Code extensions
+# code-insiders --install-extension ADD_EXTENSION_HERE
