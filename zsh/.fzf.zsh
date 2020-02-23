f0b416ba25e1f9f4bb774453ceb5f94733b16beb
Jon Friesen <jfriesen@digitalocean.com>
2020-02-23 09:36:38 -0800

    make zsh integration generic

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/jon/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/jon/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/jon/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/jon/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
