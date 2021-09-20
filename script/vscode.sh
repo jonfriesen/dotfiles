d5b4545163ec59f71ac6dfbf5bafb0c0f7e001ba
Jon Friesen <jfriesen@digitalocean.com>
2021-09-20 10:58:12 -0700

    update vscode url

--- script/vscode.sh
+++ script/vscode.sh
@@ -5,5 +5,5 @@ if [ $EUID != 0 ]; then
     exit 1
 fi
 
-wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O /tmp/code_latest_amd64.deb
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/code_latest_amd64.deb
 dpkg -i /tmp/code_latest_amd64.deb
