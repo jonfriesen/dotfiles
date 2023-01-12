2dea2e81f5a3dd55c80817deabd287478a8dbc60
Jon Friesen <jfriesen@digitalocean.com>
2023-01-12 09:40:10 -0700

    adds apps tmux config script

---
+++ scripts/apps.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+SESSIONNAME="apps"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+ then
+    tmux new-session -s $SESSIONNAME -d
+    tmux send-keys -t $SESSIONNAME "cda; nvim ." C-m
+    tmux split-window -v
+    tmux resize-pane -D 10
+    tmux send-keys -t $SESSIONNAME "cda; clear" C-m
+    tmux select-pane -t 1
+fi
+
+tmux attach -t $SESSIONNAME
