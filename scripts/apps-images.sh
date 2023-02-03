33f0d70fb47e346e7ab59955a80aa51d9840f2f6
Jon Friesen <jfriesen@digitalocean.com>
2023-02-02 18:02:06 -0700

    adds vim window movements to tmux

---
+++ scripts/apps-images.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+SESSIONNAME="apps-images"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+ then
+	tmux new-session -s $SESSIONNAME -d
+	tmux send-keys -t $SESSIONNAME "~/code/buildpacks/apps-images; nvim ." C-m
+	tmux split-window -v
+	tmux resize-pane -D 10
+	tmux send-keys -t $SESSIONNAME "~/code/buildpacks/apps-images; clear" C-m
+	tmux select-pane -t 1
+fi
+
+tmux attach -t $SESSIONNAME
