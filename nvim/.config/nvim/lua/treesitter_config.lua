29dd852e5c4b98fb6373dac73d71cd0a4785e0e3
Jon Friesen <jfriesen@digitalocean.com>
2023-02-03 09:32:22 -0700

    update tree sitter ensures

--- nvim/.config/nvim/lua/treesitter_config.lua
+++ nvim/.config/nvim/lua/treesitter_config.lua
@@ -1,10 +1,11 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "go", "gomod" },
+	ensure_installed = { "c", "go", "gomod", "svelte", "bash", "sql" },
+	sync_install = true,
 	highlight = {
 		enable = true,
 		-- there's an issue with lua highlighting right now...
 		-- the defualt highlighter is good enough for meeee
-		disable = { "lua" },
+		disable = { "lua", "help", "vim" },
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
