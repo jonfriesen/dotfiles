b41936aafb75f80db1767db802fa4854fa2bd163
Jon Friesen <jon@jonfriesen.ca>
2023-03-31 20:55:39 -0600

    removes double format code

--- nvim/.config/nvim/lua/jon/configs/lsp.lua
+++ nvim/.config/nvim/lua/jon/configs/lsp.lua
@@ -56,11 +56,6 @@ local on_attach = function(_, bufnr)
 			end
 		end,
 	})
-
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
 end
 
 -- Enable the following language servers
