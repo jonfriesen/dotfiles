bc74653351eeaadab0f17f21cd9755b4187020b4
Jon Friesen <jon@jonfriesen.ca>
2023-11-27 06:36:13 -0700

    fixes indent-blankline upgrade from v2 to v3

--- nvim/.config/nvim/lua/jon/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/jon/plugins/indent-blankline.lua
@@ -1,9 +1,21 @@
 return {
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup({
+				scope = {
+					show_start = false,
+				},
+				indent = {
+					char = "┊",
+					tab_char = "┊",
+					smart_indent_cap = true,
+				},
+				whitespace = {
+					remove_blankline_trail = true,
+				},
+			})
+		end,
+	},
 }
