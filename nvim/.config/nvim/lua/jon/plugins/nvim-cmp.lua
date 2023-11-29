ac5a34688a9fba30885b0aebfe8fbe7e9dfb7867
Jon Friesen <jon@jonfriesen.ca>
2023-11-29 12:55:51 -0700

    fixes nvim-cmp tabbing issue

--- nvim/.config/nvim/lua/jon/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/jon/plugins/nvim-cmp.lua
@@ -1,106 +1,110 @@
 return {
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer'
-    },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local ok, lspkind = pcall(require, "lspkind")
-      if not ok then
-        print "not ok"
-        return
-      end
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local ok, lspkind = pcall(require, "lspkind")
+			if not ok then
+				print("not ok")
+				return
+			end
 
-      lspkind.init()
+			-- this line handles a case where a snippet has been used but not tabbed through
+			-- causing the next tab to jump to the last used snippet location
+			luasnip.config.set_config({
+				region_check_events = "InsertEnter",
+				delete_check_events = "InsertLeave",
+			})
 
-      cmp.setup {
-        window = {
-          completion = {
-            border = "rounded",
-            scrollbar = "",
-          },
-          documentation = {
-            border = "rounded",
-            scrollbar = "",
-          },
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "nvim_lua" },
-          { name = "path" },
-          { name = "luasnip" },
-          { name = "buffer",                 keyword_length = 5 },
-          { name = "nvim_lsp_signature_help" },
-        },
-        formatting = {
-          format = lspkind.cmp_format {
-            maxwidth = 50,
-            ellipsis_char = "...",
-            with_text = true,
-            menu = {
-              buffer = "[buf]",
-              nvim_lsp = "[LSP]",
-              nvim_lua = "[api]",
-              path = "[path]",
-              luasnip = "[snip]",
-            },
-          },
-        },
+			lspkind.init()
 
-        experimental = {
-          ghost_text = true,
-        },
-      }
+			cmp.setup({
+				window = {
+					completion = {
+						border = "rounded",
+						scrollbar = "",
+					},
+					documentation = {
+						border = "rounded",
+						scrollbar = "",
+					},
+				},
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "path" },
+					{ name = "luasnip" },
+					{ name = "buffer", keyword_length = 5 },
+					{ name = "nvim_lsp_signature_help" },
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+						with_text = true,
+						menu = {
+							buffer = "[buf]",
+							nvim_lsp = "[LSP]",
+							nvim_lua = "[api]",
+							path = "[path]",
+							luasnip = "[snip]",
+						},
+					}),
+				},
 
-      -- Insert `(` after select function or method item
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      cmp.event:on(
-        "confirm_done",
-        cmp_autopairs.on_confirm_done()
-      )
-    end,
-  },
+				experimental = {
+					ghost_text = true,
+				},
+			})
+
+			-- Insert `(` after select function or method item
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
