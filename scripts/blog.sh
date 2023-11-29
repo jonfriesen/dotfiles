307da0befc366c04dce1c876c39c7e80b817d199
Jon Friesen <jon@jonfriesen.ca>
2023-11-29 12:55:41 -0700

    cleanup

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -8,10 +8,10 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 if [ $? != 0 ]
  then
 	tmux new-session -s $SESSIONNAME -d
-	tmux send-keys -t $SESSIONNAME "~/code/jon/jonfriesen.ca; nvim ." C-m
+	tmux send-keys -t $SESSIONNAME "~/code/jonfriesen.ca; nvim ." C-m
 	tmux split-window -v
 	tmux resize-pane -D 10
-	tmux send-keys -t $SESSIONNAME "~/code/jon/jonfriesen.ca; clear" C-m
+	tmux send-keys -t $SESSIONNAME "~/code/jonfriesen.ca; clear" C-m
 	tmux select-pane -t 1
 fi
 
