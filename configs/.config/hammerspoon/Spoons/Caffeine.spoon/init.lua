7d28cb7cf5d2d4955711afbaadbe6f54e64c26bd
Jon Friesen <jon@jonfriesen.ca>
2026-01-21 10:25:20 -0700

    Add timer to caffeine

--- configs/.config/hammerspoon/Spoons/Caffeine.spoon/init.lua
+++ configs/.config/hammerspoon/Spoons/Caffeine.spoon/init.lua
@@ -19,6 +19,8 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 obj.menuBarItem = nil
 obj.hotkeyToggle = nil
 obj.lockWatcher = nil
+obj.durationTimer = nil
+obj.timerEndTime = nil
 
 -- Internal function used to find our location, so we know where to load files from
 local function script_path()
@@ -33,6 +35,7 @@ end
 function obj:lockEventHandler(event)
     if event == hs.caffeinate.watcher.screensDidLock then
         if hs.caffeinate.get("displayIdle") then
+            self:cancelTimer()
             self:setState(false)
         end
     end
@@ -71,7 +74,18 @@ end
 function obj:start()
     if self.menuBarItem then self:stop() end
     self.menuBarItem = hs.menubar.new()
-    self.menuBarItem:setClickCallback(self.clicked)
+    self.menuBarItem:setMenu(function()
+        local buttons = hs.eventtap.checkMouseButtons()
+        if buttons.right then
+            -- Right click - show menu
+            return self:buildMenu()
+        else
+            -- Left click - toggle and don't show menu
+            self:cancelTimer()
+            self.clicked()
+            return {}
+        end
+    end)
     if (self.hotkeyToggle) then
         self.hotkeyToggle:enable()
     end
@@ -103,6 +117,11 @@ function obj:stop()
         self.lockWatcher:stop()
         self.lockWatcher = nil
     end
+    if self.durationTimer then
+        self.durationTimer:stop()
+        self.durationTimer = nil
+        self.timerEndTime = nil
+    end
     self.menuBarItem = nil
     return self
 end
@@ -132,6 +151,102 @@ end
 function obj:setState(on)
     hs.caffeinate.set("displayIdle", on)
     obj.setDisplay(on)
+end
+
+--- Caffeine:enableForDuration(seconds)
+--- Method
+--- Enables Caffeine for a specified duration
+---
+--- Parameters:
+---  * seconds - Number of seconds to keep Caffeine enabled
+---
+--- Returns:
+---  * None
+function obj:enableForDuration(seconds)
+    -- Cancel any existing timer
+    if self.durationTimer then
+        self.durationTimer:stop()
+        self.durationTimer = nil
+    end
+
+    -- Enable caffeine
+    self:setState(true)
+
+    -- Set timer to disable after duration
+    self.timerEndTime = os.time() + seconds
+    self.durationTimer = hs.timer.doAfter(seconds, function()
+        self:setState(false)
+        self.durationTimer = nil
+        self.timerEndTime = nil
+    end)
+end
+
+--- Caffeine:cancelTimer()
+--- Method
+--- Cancels any active duration timer
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * None
+function obj:cancelTimer()
+    if self.durationTimer then
+        self.durationTimer:stop()
+        self.durationTimer = nil
+        self.timerEndTime = nil
+    end
+end
+
+--- Caffeine:buildMenu()
+--- Method
+--- Builds the dropdown menu for the menubar item
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * A table containing the menu items
+function obj:buildMenu()
+    local currentState = hs.caffeinate.get("displayIdle")
+    local hasTimer = self.durationTimer ~= nil
+
+    local menu = {
+        {
+            title = currentState and "Disable Caffeine" or "Enable Caffeine",
+            fn = function()
+                self:cancelTimer()
+                self.clicked()
+            end
+        },
+        { title = "-" },
+        { title = "Enable for:", disabled = true },
+        { title = "5 minutes", fn = function() self:enableForDuration(5 * 60) end },
+        { title = "15 minutes", fn = function() self:enableForDuration(15 * 60) end },
+        { title = "30 minutes", fn = function() self:enableForDuration(30 * 60) end },
+        { title = "1 hour", fn = function() self:enableForDuration(60 * 60) end },
+        { title = "4 hours", fn = function() self:enableForDuration(4 * 60 * 60) end },
+        { title = "8 hours", fn = function() self:enableForDuration(8 * 60 * 60) end },
+    }
+
+    -- Show remaining time if timer is active
+    if hasTimer and self.timerEndTime then
+        local remaining = self.timerEndTime - os.time()
+        if remaining > 0 then
+            local mins = math.floor(remaining / 60)
+            local hours = math.floor(mins / 60)
+            mins = mins % 60
+            local timeStr
+            if hours > 0 then
+                timeStr = string.format("%dh %dm remaining", hours, mins)
+            else
+                timeStr = string.format("%dm remaining", mins)
+            end
+            table.insert(menu, 2, { title = timeStr, disabled = true })
+        end
+    end
+
+    return menu
 end
 
 return obj
